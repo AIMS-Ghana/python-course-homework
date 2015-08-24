b3baa7f6f6f193588cbc157a3a6267371671293a
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-24 17:58:11 +0000

    Changes made Hw.py and Hw2.py

---
+++ hw2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys 
+
+input_var = sys.argv[1]
+
+#greetings function
+def greetings(word=input_var):
+	print "Hello", word
+
+if __name__ == "__main__":
+	greetings()
+
+
+
