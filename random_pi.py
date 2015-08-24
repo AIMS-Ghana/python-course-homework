d93b67cb8715168251801b1dbc554eb8c467c93a
Carl Pearson <cap10@ufl.edu>
2015-08-24 05:35:14 -0400

    in progress random_pi imp

---
+++ random_pi.py
@@ -0,0 +1,9 @@
+import sys
+import numpy.random as npr
+from numpy import reshape
+
+seed = int(sys.argv[1])
+npr.seed(seed)
+n = int(sys.argv[2])
+samples = npr.random_sample(n)
+circ = reshape(samples, ())
