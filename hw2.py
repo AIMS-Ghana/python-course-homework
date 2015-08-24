d539c163be93e6752f3f9f0c39cd5cf617e12c9a
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-24 19:15:12 +0000

    Changes made to hw2.py

--- hw2.py
+++ hw2.py
@@ -3,9 +3,8 @@ import sys
 
 input_var = sys.argv[1]
 
-#greetings function
 def greetings(word=input_var):
-	print "Hello", word
+	print "hello", word,"!"
 
 if __name__ == "__main__":
 	greetings()
