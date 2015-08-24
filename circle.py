ce0bfe05215ddc801b103e71b286c04bfe2d981f
Carl Pearson <cap10@ufl.edu>
2015-08-24 15:04:14 -0400

    adfkgal

---
+++ circle.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+def area(r):
+    # if r negative: error
+    pass
+
+
+def perimeter(r):
+    # if r negative: error
+    pass
+
+
+import sys
+
+if __name__ == "__main__":
+    sys.arg
---
+++ triangle.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+def area(a, b, c):
+    # if a, b, c negative: error
+    # if any a+b<c: error
+    pass
+
+
+def perimeter(a, b, c):
+    # if a, b, c negative: error
+    # if any a+b<c: error
+    pass
+
+
+import sys
+
+if __name__ == "__main__":
+    sys.arg
