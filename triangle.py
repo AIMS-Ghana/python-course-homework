5cc64eb25a9365f930d354282529fcceba41d971
eugene-aims <eugene@aims.edu.gh>
2015-08-25 23:01:31 +0000

    added triangle

---
+++ triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+greeting="area= {0}, perimeter = {1}"
+
+num1 = sys.argv[1]
+num2 = sys.argv[2]
+num3 = sys.argv[3]
+# Add two numbers
+sum = (float(num1) + float(num2) + float(num3))
+s = (float(num1) + float(num2) + float(num3)) / 2
+
+# calculate the area
+product = (s*(s-float(num1))*(s-float(num2))*(s-float(num3))) ** 0.5
+
+# Display the sum
+print('area {2}'.format(num1, num2, product))
+print('perimeter {2}'.format(num1, num2, sum))
