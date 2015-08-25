40a791be81da6a555db60abeccd323c30a872522
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-25 20:46:04 +0000

    fix pull bug?

--- rectangle.py
+++ rectangle.py
@@ -16,10 +16,17 @@ def perimeter(x,y):
 	
 if __name__ == "__main__":
 
-	if len(sys.argv)!=3:
-		print "Wrong input!! provide two values"
+	if len(sys.argv)== 2:
+		#check if  int or not'3.14'.replace('.','',1).isdigit()
+		if sys.argv[1].replace('.','',1).isdigit() ==True: 
+			print(area(sys.argv[1],sys.argv[1]))
+			print(perimeter(sys.argv[1],sys.argv[1]))
+			#print(value.format(area_value,perimeter_value)) 
+		
+		else:
+			print "Wrong input!! enter a positive number!!"
 
-	else:
+	elif len(sys.argv)== 3:
 		#check if  int or not'3.14'.replace('.','',1).isdigit()
 		if sys.argv[1].replace('.','',1).isdigit() ==True and sys.argv[2].replace('.','',1).isdigit()==True: 
 			print(area(sys.argv[1],sys.argv[2]))
@@ -28,6 +35,9 @@ if __name__ == "__main__":
 		
 		else:
 			print "Wrong input!! enter a positive number!!"
+
+	else:
+		print "Wrong input!! provide two values"
 	
 
 
