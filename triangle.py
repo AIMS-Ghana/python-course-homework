cadc131fbd915b7a31f4738beff568233b9b3446
GodwinK <godwin@aims.edu.gh>
2015-08-25 22:18:49 +0000

    AN EXPLANATION OF ANY CHANGES YOU MADE

--- triangle.py
+++ triangle.py
@@ -4,7 +4,8 @@ a=float (sys.argv[1])
 b=float (sys.argv[2])
 c=float (sys.argv[3])
 s=(float(a)+float(b)+float(c))*0.5
-area=(s*(s-a)*(s-b)*(s-c))**0.5
-perimeter= (a)+(b)+(c)
-#print "area " + str(area)
+p= (a)+(b)+(c)
+area=(p*0.5*(p-a)*(p-b)*(p-c))**0.5
+
+print "area " + str(area)
 print "perimeter " ,p
