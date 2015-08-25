1e5e1b270b5f7658c14b9866225b003552f70fae
Innocenter <innocenter@aims.edu.gh>
2015-08-25 21:33:14 +0000

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,14 +2,25 @@
 import sys
 import math
 
-#print(sys.argv[1])
+import sys
+input_area="{0}"
+input_perimeter="{0}"
+def area(x):
+    area_value=math.pi*float(x)*float(x)
+    return area_value
+def perimeter(x):
+    perimeter_value=math.pi*2*float(x)
+    return perimeter_value
 
-r=int(sys.argv[1])
-if(r<0):
-	print("Error, you have been type an negative number. Retry")
-	pass
-else:
-	area=math.pi*r**2
-	perimeter=math.pi*2*r
-	print"area ", area
-	print "perimeter",perimeter
+
+if __name__ == "__main__":
+
+    if len(sys.argv) !=2:
+        print "wrong inputs"
+
+    else:   
+        if sys.argv[1].replace('.','',1).isdigit()==True:
+            print "area", area(sys.argv[1])
+            print "perimeter", perimeter(sys.argv[1])
+        else :
+		print "have atleast one value"
