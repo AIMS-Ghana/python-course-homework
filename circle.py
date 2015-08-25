32acd03f64cb9931b3a57c60b4abfa2dc9debe6b
Grace-aims <glorie@aims.edu.gh>
2015-08-25 21:50:41 +0000

    AN EXPLANATION OF ANY CHANGES YOU MADE

---
+++ circle.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+from math import pi
+import sys
+R=float(sys.argv[1])
+if (R<=0):
+	print("error")
+else:
+	print("The perimeter is:",2*pi*R)
+	print("The area is:",pi*r**2)
