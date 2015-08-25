6b051dd69014f253486123d465afb0353fdce3cc
bernard-aims <bernard@aims.edu.gh>
2015-08-25 21:12:14 +0000

    was able to solve the basic problem but could not satisfaction other condition. for instance when is it or a wrong value is enter

---
+++ rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import sys
+
+area = float(sys.argv[1])*float(sys.argv[2])
+Perimeter =(2*float(sys.argv[1])+2*float(sys.argv[2]))
+print "Area "+ str(area)
+print "Perimeter "+ str(Perimeter)
