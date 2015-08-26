9e3db44888abcb2a8b96e8c3d8f2a866838eeb71
vanemakori <vanemakori@users.noreply.github.com>
2015-08-26 13:39:57 +0000

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
-import sys
 import math
-input_area="{0}"
-input_perimeter="{0}"
-def area(x):
-	area_value=math.pi*float(x)*float(x)
-	return area_value
-def perimeter(x):
-	perimeter_value=2*math.pi*float(x)
-	return perimeter_value
 
 
-if __name__ == "__main__":
 
-	if len(sys.argv) !=2:
-		print "wrong inputs"
-	else:	
-		if sys.argv[1].replace('.','',1).isdigit()==True:
-			print "area", area(sys.argv[1])
-			print "perimeter", perimeter(sys.argv[1])
-		else:
-			print "have atleast one value"
+def check(a):
+	assert a>0, "enter positive numbers"
+
+def area(a):
+	check(a)
+	result= math.pi*a*a
+	return result
+def perimeter(a):
+	check(a)
+	value=2*math.pi*a
+	return value
+import sys
+if __name__=="__main__":
+	a=float (sys.argv[1])	
+	print("area{}, perimeter{}".format(area(a), perimeter(a)))
