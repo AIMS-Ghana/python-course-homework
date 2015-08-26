39fbc03850c97e5fc0a027c1f8272510677bd083
vanemakori <vanemakori@users.noreply.github.com>
2015-08-26 13:43:55 +0000

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 import math
 
+
+
+def check(a,b,c):
+	assert a>0 and b>0 and c>0, "enter positive numbers"
+	s=a+b
+	d=a-b
+	assert (s>c) and (c>d), "violated the triangle equilateral rule"
+def perimeter(a,b,c):
+	check(a,b,c)
+	value=a+b+c
+	return value
+def area(a,b,c):
+	#heros formula
+	check(a,b,c)
+	value=(a+b+c)/2
+	result= math.sqrt(value*(value-a)*(value-b)*(value-c))
+	return result
+
 import sys
-input_area="{0}"
-input_perimeter="{0}"
-def area(x,y,z):
-	sum_value=(float(x)+float(y)+float(z))/2
-    	area_value=(sum_value*(sum_value-float(x))*(sum_value-float(y))*(sum_value-float(z)))**0.5
-    	return area_value
-def perimeter(x,y,z):
-    	perimeter_value=float(x)+float(y)+float(z)
-   	return perimeter_value
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv) !=4:
-        print "wrong inputs"
-
-    else:   
-        if sys.argv[1].replace('.','',1).isdigit()==True:
-            print "area",  area(sys.argv[1],sys.argv[2],sys.argv[3])
-            print "perimeter",  perimeter(sys.argv[1],sys.argv[2], sys.argv[3])
-        else :
-		print "have atleast three values"
+if __name__=="__main__":
+	a=float (sys.argv[1])	
+	b=float (sys.argv[2])
+	c=float (sys.argv[3])
+	print("area{}, perimeter{}".format(area(a,b,c), perimeter(a,b,c)))
