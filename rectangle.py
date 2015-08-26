89d75c8a269740852ab85a0c476b166c6410cc0b
vanemakori <vanemakori@users.noreply.github.com>
2015-08-26 13:41:09 +0000

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
-#area of a rectangle
+#area of a triangle
 import math
 
+
+
+def check(a,b):
+	assert a>0 and b>0, "enter positive numbers"
+
+def area(a,b):
+	check(a,b)
+	result=a*b
+	return result
+def perimeter(a,b):
+	check(a,b)
+	value=2*(a+b)
+	return value
 import sys
-input_area="{0}"
-input_perimeter="{0}"
-def area(x,y):
-    	area_value=float(x)*float(y)
-    	return area_value
-def perimeter(x,y):
-    	perimeter_value=2*(float(x)+float(y))
-   	return perimeter_value
+if __name__=="__main__":
+	a=float (sys.argv[1])	
+	b=float (sys.argv[2])
+	print("area{}, perimeter{}".format(area(a,b), perimeter(a,b)))
 
 
-if __name__ == "__main__":
-    if len(sys.argv) ==2:
-	print "area",  area(sys.argv[1])
-        print "perimeter",  perimeter(sys.argv[1])
-    
-    elif len(sys.argv) !=3:
-        print "wrong inputs"
-    
-    else:   
+
         if sys.argv[1].replace('.','',1).isdigit()==True:
             print "area",  area(sys.argv[1],sys.argv[2])
             print "perimeter",  perimeter(sys.argv[1],sys.argv[2])
