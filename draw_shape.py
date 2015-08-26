c61cdb87a434482954aa1efc846e073936c0c52f
emascuryo <sowusu@aims.edu.gh>
2015-08-26 22:58:53 +0000

    Draw_shape assignment submission

---
+++ draw_shape.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from math import sqrt
+import sys
+from turtle import *
+import shapes
+
+def draw_figure(area):
+		
+	
+if __name__ == "__main__":
+	draw_figure("TRIANGLE",100)
+	draw_figure("SQUARE",100)
+	
