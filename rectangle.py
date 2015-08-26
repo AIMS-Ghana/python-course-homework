a88fb152d02cdae305aadbadaba33a1347ea86ac
kunda-aims <kunda@aims.edu.gh>
2015-08-26 01:32:37 +0000

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 import sys
-value="area {0}!\nperimeter {1}!"
+value="area {0}\nperimeter {1}"
 area=""
 perimeter=""
 if len(sys.argv)==3:
-	#check if input is a number and length of input is 2 
 	area=float(int(sys.argv[1]) * int(sys.argv[2]))
 	perimeter=2 * float(sys.argv[1]) + 2 * float(sys.argv[2])
 	print (value.format(str(area),str(perimeter)))
- 
+ elif len(sys.argv)==2:
+	area=float(int(sys.argv[1])) ** 2
+	perimeter=float(int(sys.argv[1])) * 4
+ 	print (value.format(str(area),str(perimeter)))
 else:
-	if len(sys.argv) >2:  
+	if len(sys.argv) >3:  
 		value="wrong input! two values required!"
 		print (value)
-      
+	if len(sys.argv)==1:
+		value="wrong input! two values required!"
+		print (value)
