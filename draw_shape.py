0cf0b716348a615a9e8d7cf72bb555a499703c18
eugene_aims <eugene@aims.edu.gh>
2015-08-26 19:00:53 +0000

    updated draw_shapes

---
+++ draw_shape.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+from turtle import *
+import shape
+if __name__=="__main__":
+	a=sys.argv[1]
+	b=sys.argv[2]
+	if a=="square":
+		forward(side3(b))
+		left(90)
+		forward(side3(b))
+		left(90)
+		forward(side3(b))
+		left(90)
+		forward(side3(b))
+		done()
+	elif
+		 a=="Equilateral triangle"
+			forward(side2(b))
+			left(120)
+			forward(side2(b))
+			left(120)
+			forward(side2(b))
+	elif
+		a=="Circle"
+		circle(side1(b))
+	else
+		print("error, wrong shape")	
+	
+
+
+
+
+
