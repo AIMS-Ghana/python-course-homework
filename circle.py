d50cfe531fdd044099ad985c4ebeabf79f84f245
janayan <janyan@aims.edu.gh>
2015-08-26 20:36:23 +0000

    submission

--- circle.py
+++ circle.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 
+#getting the input from the user
+
+def check(x):
+ assert x>0, "Entered a negative number"
+
+def area (x):
+ check(x)
+ return math.pi*(x**2)
+
+def perimeter (x):
+ check(x)
+ return 2*math.pi*x  
+
 import sys
 import math
 
+if __name__=="__main__":
 #getting the input from the user
+ x=float(sys.argv[1])
 
-x=float(sys.argv[1])
+ print"area " " {}, perimeter" " {}".format(area(x),perimeter(x))
 
-if x>0:
- A=math.pi*(x**2)
- P=2*math.pi*x 
- print"area ",A
- print"perimeter ",P
-else:
- print"Error!"
- print"Either a negative number."
+
