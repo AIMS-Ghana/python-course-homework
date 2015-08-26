fc70ab5ff9a7acccbb9c76e69cc27976e685bf54
nganza <dative@aims.edu.gh>
2015-08-26 13:58:06 +0000

    anytime

--- triangle.py
+++ triangle.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+import math
+def check(a,b,c):
+	assert (a>0) & (b>0) & (c>0), "entered negative dimension"
+	s= a+b
+	d= a-b
+	assert (s>c) & (d<c),"violetd triangle inequality"
+def area(a,b,c):
+	check(a,b,c)
+        k=(a+b+c)/2
+        res = math.sqrt(k*(k-a)*(k-b)*(k-c))
+        return res
+def perimeter(a,b,c):
+    check(a,b,c)
+    return a+b+c 
 import sys
-##if len(sys.argv)== 3
-##	print(sys.argv[1]* sys.argv[2] 
-b=float(sys.argv[1]) 
-w=float(sys.argv[2])
-h=float(sys.argv[3])
-area=(b*w)/2
-perimeter=b+w+h
-print("area is equal to",area)
-print("perimeter is equal to",perimeter)
+if __name__ == "__main__":
+    a= float(sys.argv[1])
+    b= float(sys.argv[2])
+    c= float(sys.argv[3])
+    print("area{}, parimeter{} ".format(area(a,b,c),perimeter(a,b,c)))
+
+
 
 
