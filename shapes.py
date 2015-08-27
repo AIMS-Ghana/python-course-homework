6993d6292a9eea5134f1b9227762b219716bb035
chilperic <chilperic@aims.edu.gh>
2015-08-27 10:18:35 +0000

    keeping up to date

--- shapes.py
+++ shapes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import math
+import math #import mathematics package
 pi=3.14
 def check(area):
   	assert (area>0) , "enter positive  number"
@@ -8,27 +8,31 @@ def check(area):
   	
 def  shape(area):
 	check(area)
-	if  a == "TRIANGLE":
- 		side= math.sqrt((area*4)/math.sqrt(3))
+	if  a == "TRIANGLE": 
+ 		side= math.sqrt((area*4)/math.sqrt(3)) # side of equileteral triangle when I know area
 		return side
 	elif a== "SQUARE":
-		side= math.sqrt(area)
+		side= math.sqrt(area) # side of square when I know area
 		return side
 
 	elif  a== "CIRCLE" :		
-		radius = math.sqrt(area/pi)
+		radius = math.sqrt(area/pi) # radius of circle when I know area
 			
 			
   		return radius
 
-import sys 
+import sys #import systeme 
 
 if __name__== "__main__":
    	a= str (sys.argv[1])
 	area= float(sys.argv[2])
-	if a=="TRIANGLE" or a== "SQUARE":
-  	 	print "size:",shape(area)
-	elif a== "CIRCLE":
- 		print "raduis:",shape(area)
+	if a=="TRIANGLE"  :
+  	 	print "the equilateral triangle of area: ",area , "size: " ,shape(area)
+	elif  a== "SQUARE":
+		print "the Square of area: ",area , "size: " ,shape(area)
 
+	elif a== "CIRCLE":
+ 		print "the circle  of area: ",area , "radius: " ,shape(area)
+	else :
+		print "invalid error try again "
 
