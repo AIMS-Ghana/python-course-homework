2d7066400c45621f8b25f894ef3c649fe0d0c870
eunice-aims <eunice@aims.edu.gh>
2015-08-27 22:14:42 +0000

    message....

---
+++ draw_shape.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# Filename: draw_shape.py
+
+import shape 
+import sys
+import turtle
+
+turtle.forward(side)
+turtle.left(98)
+turtle.forward(side)
+turtle.left(98)
+turtle.forward(side)
+turtle.left(98)
+turtle.forward(side)
+turtle.left(98)
+turtle.end_fill()
+
+turtle.exitonclick()
+
+figs[shape](dim0
+
+
+
+
+
+
+
+
+
+
+
+
