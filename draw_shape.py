4eb032871ec5fd205bf974a5988690a4f4e053d3
Racheal-aims <racheal@aims.edu.gh>
2015-08-27 17:08:40 +0000

    I was unable to finish the work because I need to understand the coding better

--- draw_shape.py
+++ draw_shape.py
@@ -1,45 +1,60 @@
 #!/usr/bin/python
 import turtle 
 
+import shapes   
+
 #draw a triangle
-def triangle_sharper(length):
-	turtle.forward(length)
+def triangle_shaper(side):
+	turtle.begin_fill()
+	turtle.color("yellow")
+	turtle.forward(side)
 	turtle.left(120)
-	turtle.forward (length)
+	turtle.forward (side)
 	turtle.left(120)
-	turtle.forward(length)
+	turtle.forward(side)
 	turtle.left(120)
-	turtle.left(length)
-	turtle.exitonclick()
+	turtle.left(side)
+	turtle.end_fill()
+	turtle.left(60)
+	
 
 #draw a circle
-def circle_sharper(length):
-	turtle.circle(length)
-	turtle.exitonclick()
+def circle_shaper(radius):
+	turtle.begin_fill()
+	turtle.color("red")
+	turtle.circle(radius)
+	turtle.end_fill()
+	turtle.left(60)
+	
 
 #draw a square
-def square_sharper(length):
+def square_shaper(side):
+	turtle.left(60)
+	turtle.begin_fill()
+	turtle.color("green")
 	turtle.right(90)
-	turtle.forward(length)
+	turtle.forward(side)
 	turtle.right(90)
-	turtle.forward(length)
+	turtle.forward(side)
 	turtle.right(90)
-	turtle.forward(length)
+	turtle.forward(side)
 	turtle.right(90)
-	turtle.forward(length)
+	turtle.forward(side)
+	turtle.end_fill()
 	turtle.done()
-if __name__ == "__main__":
-	square_sharper(50)
-	triangle_sharper(50)
-	circle_sharper(10)
 	
-def draw(name,length):
+if __name__ == "__main__":
+	square_shaper(50)
+	triangle_shaper(50)
+	circle_shaper(10)
+	
+def draw(name,side,color):
 	if name =="TRIANGLE":
-		triangle_sharper(length)
+		triangle_shaper(side,color('yellow'))
 	if name =="SQUARE":
-		square_sharper(length)
+		square_shaper(side,color('green'))
 	if name =="CIRCLE":
-		circle_sharper(length)
+		circle_shaper(side,color('red'))
 	else:
 		pass
 
---
+++ midpoint.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import math
+
+
+def int(n):
+    return (a+(i-0.5)*h)
+
+
+
+
