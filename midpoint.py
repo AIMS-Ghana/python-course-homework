161974166f566027983e06a24629777efde60498
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-27 20:15:27 +0000

    changes made to midpoint.py works with try_integrator.py

--- midpoint.py
+++ midpoint.py
@@ -4,14 +4,13 @@
 
 #integration function
 def integrate(f, nbins):
-
-    
-    sum = 0.0
-    for n in range(nbins-1):  # [0, 1, ... nbins-1]
+    sum_t = 0.0
+    for n in range(len(nbins)-1):  # [0, 1, ... nbins-1]
 	width = nbins[n+1] - nbins[n]
-	height = f(float(nbins[n+1] + nbins[n])/2)
-        sum = sum + width*height
+	sum_l = nbins[n+1] + nbins[n]
+	height = f(float(sum_l)/2)
+        sum_t = sum_t + width*height
 
-    return sum
+    return sum_t
 
 
