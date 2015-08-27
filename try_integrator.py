e53786cfb6b09c0300eeb493bd569af0e5a3f782
Carl Pearson <cap10@ufl.edu>
2015-08-27 12:30:02 -0400

    actually implement the check harness

--- try_integrator.py
+++ try_integrator.py
@@ -1,10 +1,11 @@
 #!/usr/local/bin/python3
 
+import numpy as np
 import midpoint
 
 def intf(x):
-    ...
+    return np.exp(x)
 
-rangex = [...]
+rangex = np.linspace(0, 10, 100, endpoint=True)
 
 print(midpoint.integrate(intf, rangex))
