c92691b035283e715a3d82e0c586a48852b5185b
Leonard Okyere Afeke <leonard@aims.edu.gh>
2015-08-27 09:50:39 +0000

    Committing file shapes.py

--- shapes.py
+++ shapes.py
@@ -2,5 +2,10 @@
 from math import pi
 
 
-circle = lambda area : (area/pi)**0.5
-triangle lambda area :
+circle = lambda a : (a/pi)**0.5
+triangle = lambda a : 2*(a/(3**0.5))**0.5
+square = lambda a : a**0.5
+
+
+shapes = {'circle' : cicrle, 'triangle' : triangle, 'square' : square}
+ 
