442d1e5bb70757672e1218a96999e4c428730268
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-27 09:59:31 +0000

    Update render_shapes.py

--- render_shapes.py
+++ render_shapes.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 from draw_shape import draw
 
-draw("CIRCLE", 100)
-draw("SQUARE", 100)
-draw("TRIANGLE", 100) # equilateral triangle
+draw("CIRCLE", 1000)
+draw("SQUARE", 1000)
+draw("TRIANGLE", 1000) # equilateral triangle
