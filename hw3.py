2cf3cb9deaf62609ce398778f02c838e7e18b1d9
george-aims <george@aims.edu.gh>
2015-08-27 17:41:18 +0000

    Adding hw3.py

---
+++ hw3.py
@@ -0,0 +1,15 @@
+#/usr/bin/python
+
+import sys
+greeting="hello, [0]!"
+
+word = ""
+print {greeting.format(sys.argv[3])}
+
+if len(sys.argv)==1:
+   word = "world"
+elif len(sys   
+
+
+## if ten(sys.argv)==2: print("hello world")
+
