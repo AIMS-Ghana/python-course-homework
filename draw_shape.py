fd687e4aee700de15dbd6415f778f577d480c4e6
vanemakori <vanemakori@users.noreply.github.com>
2015-08-27 07:44:03 +0000

    Update draw_shape.py

--- draw_shape.py
+++ draw_shape.py
@@ -41,3 +41,51 @@ def main():
 	draw_circle(radius)
 if __name__=="__main__":
 	main()
+#!/usr/bin/python
+#draw shapes of circle, equilateral triangle and square
+import sys
+import turtle
+import shapes
+import math
+def draw_triangle(side):
+    window = turtle.Screen()
+  
+    turtle.forward(side) 
+    turtle.color('blue')
+    turtle.left(120)
+    turtle.forward(side)
+    turtle.color('blue')
+    turtle.left(120)
+    turtle.forward(side)
+    window.exitonclick()
+
+def draw_square(width):
+    win = turtle.Screen()
+    turtle.left(90)
+    turtle.forward(width)
+    turtle.color('green')
+    turtle.left(90)
+    turtle.forward(width)
+    turtle.color('green')
+    turtle.left(90)
+    turtle.forward(width)
+    turtle.color('green')
+    turtle.left(90)
+    turtle.forward(width)
+    turtle.color('green')
+    turtle.left(90)
+    win.exitonclick() #to exit
+
+def draw_circle(rad):	
+	window = turtle.Screen()
+	circle_drw = turtle.Turtle()
+	circle_drw.speed(20)
+        circle_drw.circle(rad)
+	window.exitonclick() 
+
+	
+
+if __name__=="__main__":
+	draw_triangle(100)
+	draw_square(100)
+	draw_circle(100)
