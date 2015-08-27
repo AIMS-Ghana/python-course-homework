cce3629788cdf9346eba25ed4388424c1da1f529
Carl Pearson <cap10@ufl.edu>
2015-08-27 12:43:45 -0400

    some work

---
+++ midpoint.py
@@ -0,0 +1,12 @@
+def integrate(f, rangex):
+    ss = 0
+    for i in range(len(rangex)-1):
+        w = rangex[i+1] - rangex[i]
+        h = f((rangex[i+1] - rangex[i])/2)
+        ss = ss + w*h
+    return ss
+    # get range widths
+    # get range midpoints
+
+if __name__ == "__main__":
+    print(integrate(lambda x:x, range(5)))
