ad3aa5f9fca5c537d5e0edefc3bcd379f8435190
kelechi-aims <kelechi@aims.edu.gh>
2015-08-27 17:40:42 +0000

    corrections made to the draw_shape.py

--- draw_shape.py
+++ draw_shape.py
@@ -1,8 +1,45 @@
 #!/usr/bin/python
-import sys
-import maths
+import math
 import shapes
 import turtle
 
+def circle (radius):
+    turtle.circle(radius)
 
-#drawing triangle with area 
+def ngon (n, side):
+    turn= 180-(n-2)*180.0/n
+    for i in range (n-1):
+        turtle.forward(side)
+        turtle.right(turn)
+    turtle.forward(side)
+
+def triangle(side):
+    ngon(3,side)
+
+def square(side):
+    ngon(4,side)
+
+figs={
+    'CIRCLE':circle,
+    'SQUARE':square,
+    'TRIANGLE':triangle
+}
+
+def draw(shape, area, fill="black", cont=False):
+    dim = shapes.figure_check(shape, area)
+    turtle.color
+    figs[shape](dim)
+    turtle.exitonclick()
+
+
+import sys
+
+if __name__=="__main__":
+   
+   side_triangle = shapes.figure_check('TRIANGLE',70000)  
+   draw('TRIANGLE',side_triangle)
+   side_square = shapes.figure_check('SQUARE',70000)  
+   draw('SQUARE',side_square)
+   radius_circle = shapes.figure_check('CIRCLE',70000)  
+   draw('CIRCLE',radius_circle)
+
