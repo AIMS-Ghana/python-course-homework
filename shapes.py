4c120d21af20b799db4801d9da466421a10f84b6
mumjudith <danny@aims.edu.gh>
2015-08-27 20:28:26 +0000

    adding shapes.py

---
+++ shapes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+import math
+def polygon_check(polygon_name,polygon_area):
+if polygon_name,upper()=='triangle'
+
+# find side
+side=math,sqrt(float(polygon_area)/float(math,sqrt(3)))
+print "/n equilateral triangle,area",polygon,"side'side,"..../n
+values=[polygon_area,side]
+return values
+elif name.upper()=='square'
+#find side
+side=math.sqrt(float(polygon_area))
+print '\nsquare,area'polygon,"side'side,"...\n
+elife polygon_name.upper()=='circle
+	radius=math.sqrt(float(polygon_area)/math.pi)
+	print"\ncircle,'area.polygon_area.radius.radius....\n
+else:	print'\n...error indicating unkown shapes..
+if_name_""_main_"
+	if leng(sys.argv)==1
+	print'\n...error indicating no input..\n'
+	else:polygon_check(sys.argv[1],sys.argv[2])
+	
