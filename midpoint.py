66bc06275df5d58fc8b5abf0bab8516e6fdaa149
nganza <dative@aims.edu.gh>
2015-08-27 23:17:47 +0000

    midpoint

--- midpoint.py
+++ midpoint.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 import math
+import midpoint
 def integrate(a,b):
 	sum= 0.0
-	for n in range(len(b)-1):
+	for n in range(len(b)- 1):
 		midpoint= float(b[n+1]+[n]/2)
 		width= b[n+1]-b[n]
 		height= a(midpoint)
