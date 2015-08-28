dda3a09abc991c54c3513765a033f6f2d4abb125
chilperic <chilperic@aims.edu.gh>
2015-08-28 13:37:21 +0000

    pre merge

---
+++ bisection.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+
+	
+epsilon = 0.00000000000000001
+
+def root(f,rangex):
+        l=rangex[0]
+	r=rangex[1]
+	while abs(l-r)> epsilon :
+       		x=(l+r)/2.0
+		if f(x)== 0 :
+			print(x)
+		elif f(l)*f(x)<0 :
+   			r=x
+		else :
+   			l=x
+		print x
+	        break
--- triangle.py
+++ triangle.py
@@ -22,5 +22,4 @@ if __name__== "__main__":
    	a= float(sys.argv[1])
    	b= float(sys.argv[2])
    	c= float(sys.argv[3])
-	print 6
   	print ("area {}, perimeter {}".format(area(a,b,c),perimeter(a,b,c)))
