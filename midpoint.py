3523d9ea5799da3270011f1b8b34344b65d7ef25
Innocenter <innocenter@aims.edu.gh>
2015-08-28 00:11:59 +0000

    Update midpoint.py

---
+++ midpoint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#to calculate the midpoint,function to find integrators
+
+
+
+def integrate(f, nbins):
+    s=0.0
+
+    for n in range(len(nbins)-1):
+        width=nbins[n+1]-nbins[n]
+        midp=float(nbins[n+1]+nbins[n]/2)
+        height=f(midp)
+        s=midp+width*height
+    return s
