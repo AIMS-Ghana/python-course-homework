0bf2f76613b945e6d052c8fe012467fa42c6e43d
kunda-aims <kunda@aims.edu.gh>
2015-08-28 00:50:40 +0000

    mid_point.py

---
+++ midpoint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def intergrateFunc(a,b):
+	# calculate the integral from a to b of function f using the midpoint rule
+	for x in range (len(b-1)):
+		h=float(b-a)/n
+		assert h > 0
+		assert type(n) == int
+		x += (a + h/2)*n
+	for n in range[a,b]:
+		return h*(x)
+
+def integrate(f,x):
+	
+	return f(x)
