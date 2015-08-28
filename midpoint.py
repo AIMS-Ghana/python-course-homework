46542e210fe6e8408ac33b0a9998d6f15a785546
rebecca-aims <rfofie@aims.edu.gh>
2015-08-28 06:18:09 +0000

    update of midpoint.py

--- midpoint.py
+++ midpoint.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 import sys  
 
-## midpoint formular ##
-   integrate (k,a,b)
-    k= int(k)
-    a=float(a)
-    b=float(b)
-   import math
-   k=float(b-a)/k)
-   m=0
-   if ((a.b) &(k<0):
-     print("error,a must be < b & k > 0")
-
-   else:
-    for i in range(n-1):
-    h=(a+(i+0.5*h))
-    m=m+k*math.exp(h)
+# finding midpoint
+def integrate(f,b):
+   for n in range (n-1):
+      h=(b[n]-b[n-1])/n
+      width=(b[n]-b[n-1])
+      midpoint=(b[n]-b[n-1])/2
+      y=midpoint+width*height
+   return y
