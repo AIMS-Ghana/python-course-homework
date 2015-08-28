061d29142c32e2dde97e44fce975b1957ab63db8
Racheal-aims <racheal@aims.edu.gh>
2015-08-28 01:14:04 +0000

    updated integrated midpoint rule

--- midpoint.py
+++ midpoint.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 
-import math
+def integMid(a, b, f, nbins=10):
+    '''Return the integral from a to b of function f using the midpoint rule
 
+    >>> integMid(0, 1, f, 4)
+    0.828125
+    '''
+    h = float(b-a)/nbins
+    assert h > 0
+    assert type(nbins) == int
+    
+    sum = 0.0
+    x = a + h/2                  # first midpoint
+    while (x < b):
+        sum += h * f(x)
+        x += h
 
-def int(n):
-    return (a+(i-0.5)*h)
-
+    return sum
 
 
 
