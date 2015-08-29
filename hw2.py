4aa0abe574c2317282b97eeb1f8be1474ce66ec1
sarobidy19-aims <andriahermanana@aims.edu.gh>
2015-08-29 19:50:54 +0000

    more merge fixes

--- hw2.py
+++ hw2.py
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-#!/usr/bin/python
+#!/usr/bin/env python3
 import sys
 print("hello,"+sys.argv[1]+"!")
-=======
-
-import sys
-print"hello "+sys.argv[1]
->>>>>>> Stashed changes
--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 git stash save
 git fetch origin master
 git merge origin/master --no-edit
 git stash apply
+=======
+git fetch origin master
+git merge origin/master --ff --no-edit
+>>>>>>> Stashed changes
