9a175042bb89f272dd054cf02ef79d692294007d
sarobidy19-aims <andriahermanana@aims.edu.gh>
2015-08-29 19:51:35 +0000

    more merge fixes

--- update.sh
+++ update.sh
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
 git stash save
 git fetch origin master
 git merge origin/master --no-edit
 git stash apply
-=======
-git fetch origin master
-git merge origin/master --ff --no-edit
->>>>>>> Stashed changes
