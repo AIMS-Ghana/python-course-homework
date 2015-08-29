a70fc022669d9193473b4bdb5c4670b2a14ab7e3
Carl Pearson <cap10@ufl.edu>
2015-08-29 08:13:54 -0400

    add trap, secant outline

---
+++ trapezoid.py
@@ -0,0 +1,15 @@
+def integrate(f, rangex):
+    ss = 0
+    for i in range(len(rangex)-1):
+        a = rangex[i]
+        b = rangex[i+1]
+        w = b - a
+        fa = f(a)
+        fb = f(b)
+        ss = ss + w*(fa+fb)/2
+    return ss
+    # get range widths
+    # get range midpoints
+
+if __name__ == "__main__":
+    print(integrate(lambda x:x, thing))
