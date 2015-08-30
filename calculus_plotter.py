bb800154e6c067f60cf129db4c629947a61bff85
kunda-aims <kunda@aims.edu.gh>
2015-08-30 16:57:39 +0000

    final edited

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -1,56 +1,28 @@
 #!/usr/bin/env python
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import derivative as deriv
-import scipy.integrate as integ
+from scipy.integrate import odeint
 
-f = lambda x: x
+def func(g,x):
+	return g(x)
 
-x = np.linspace(-np.pi,np.pi,256,endpoint = True) # range of x values on graph
-y,dydx = x,deriv(f,x)  # define the y values on graph
-plt.plot(x,y,'r') # plot x against y
-plt.plot(x,dydx,'g')
-plt.xlabel('x-axis') # label x-axis
-plt.ylabel('y-axis') # label y-axis
-plt.show() # show graph
+def plotter(f):
+	def h(I,x):
+		return f(x)
+	init = 0
+	x = np.arange(-10.0, 20.0, 0.01) # range of x values on graph
+	y,dydx,i = func(f,x),deriv(f,x),odeint(h,init,x)  # define the y values on graph
+	plt.plot(x,y,'r', label='f(x)') # plot x against y
+	plt.plot(x,dydx,'g', label= 'dydx')
+	plt.plot(x,i,'b', label = 'integral')
+	plt.xlabel('x-axis') # label x-axis
+	plt.ylabel('y-axis') # label y-axis
+	plt.ylim(-5,5)
+	plt.legend()
+	plt.show() # show graph
 
-f = lambda x: 1 - np.exp(-x)
 
-x = np.linspace(-np.pi,np.pi,256,endpoint = True) # range of x values on graph
-y,dydx = 1 - np.exp(-x),deriv(f,x)  # define the y values on graph
-plt.plot(x,y,'r') # plot x against y
-plt.plot(x,dydx,'g')
-plt.xlabel('x-axis') # label x-axis
-plt.ylabel('y-axis') # label y-axis
-plt.show() # show graph
-
-f = lambda x: np.exp(5*x)
-
-x = np.linspace(-np.pi,np.pi,256,endpoint = True) # range of x values on graph
-y,dydx = np.exp(5*x),deriv(f,x)  # define the y values on graph
-plt.plot(x,y,'r') # plot x against y
-plt.plot(x,dydx,'g')
-plt.xlabel('x-axis') # label x-axis
-plt.ylabel('y-axis') # label y-axis
-plt.show() # show graph
-
-f = lambda t: np.sin(t) + np.cos(t)
-
-t = np.linspace(-np.pi,np.pi,256,endpoint = True) # range of x values on graph
-y,dydx = np.sin(t) + np.cos(t),deriv(f,t)  # define the y values on graph
-plt.plot(t,y,'r') # plot x against y
-plt.plot(t,dydx,'g')
-plt.xlabel('x-axis') # label x-axis
-plt.ylabel('y-axis') # label y-axis
-plt.show() # show grap
-
-f = lambda t: np.sin(t)**2
-
-t = np.linspace(-np.pi,np.pi,256,endpoint = True) # range of x values on graph
-y,dydx = np.sin(t)**2,deriv(f,t)  # define the y values on graph
-plt.plot(t,y,'r') # plot x against y
-plt.plot(t,dydx,'g')
-plt.xlabel('x-axis') # label x-axis
-plt.ylabel('y-axis') # label y-axis
-plt.show() # show grap
-
+if __name__ == "__main__":
+	plotter(np.sin)
--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
-import calculus_plotter
-from calculus_plotter import plot
-plot('easy')
+import fun_plots as fp
+import calculus_plotter as cp
+
+for i in range (0,5):
+	cp.plotter(fp.funclist[i])
+ 
