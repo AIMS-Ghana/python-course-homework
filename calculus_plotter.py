b55eafaaab7ef70a5ff0d5e0ff97a58a5754327c
fred-aims <fred@aims.edu.gh>
2015-08-30 11:59:31 +0000

    I managed to make all the plots

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
 
 from pylab import *
-import numpy
-import math
+from scipy.misc import derivative
+from scipy.integrate import odeint
 
-X = arange(-5,6,0.001)
-title("A graph of a function with it's derivative and integral")
-xlabel("X")
-ylabel("Y")
-plot(X,sin(X), '--r', lw = 2, ms = 3, label = "f'(x)")
-plot(X,cos(X), '--b', lw = 2, ms = 3, label = "f(x)")
-legend()
+#import fun_plots as fp
+#from simpy import integrate
 
+funames = [r'$f(x) = x$' , r'$f(x) = 1-e^{-x}$',r'$f(x) = e^{5x}$',r'$f(x) = \sin(t) + \cos(t)$',r'$f(x) = \sin^{2}(t)$']
+
+def plotter(fxn,i):
+    figure(i+1),grid(True)
+    title(" A graph of {0} with it's derivative and integral".format(funames[i]))
+    xlabel("X")
+    ylabel(r'\f(x) , \f\prime(x) , \$\int{f(x)}$')
+    xticks(arange(0,10,1))
+
+    X = arange(0,10,0.01)
+
+    def k(I,X):
+	return fxn(X)
+
+    Dfxn = derivative(fxn,X,dx=1,n=1)
+    Ifxn = odeint(k,0,X)
+
+    plot(X,fxn(X), 'r', lw = 2, ms = 3, label = "f(x)")
+    plot(X,Dfxn, 'b', lw = 2, ms = 3, label = "f'(x)")
+    plot(X,Ifxn, 'g', lw = 2, ms = 3, label = r'$\int{f(x)}$')
+    legend()
+    show()
---
+++ make_fun_plots.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import calculus_plotter
+import fun_plots
+
+
+for i in range(0,5):
+    calculus_plotter.plotter(fun_plots.funclist[i],i)
