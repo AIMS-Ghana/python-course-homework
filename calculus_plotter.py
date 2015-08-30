dc6e1b14ee1c9da823125b9384f5a60fc2977aa5
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-30 11:43:19 +0000

    calculus plotter works with make_fun_plots...

---
+++ calculus_plotter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+
+import midpoint
+import numpy as np
+from sympy import *
+import matplotlib.pyplot as pyplot
+from numpy import gradient
+import scipy.integrate as integrate
+
+
+
+#test functions
+def sine_and_cos(t):
+    return np.sin(t) + np.cos(t)
+
+
+def easy(x):
+    return x
+
+
+'''
+1. ploting just the function
+2. ploting the derivative of the function
+3. ploting the integral of the function
+'''
+
+
+def functions_plot(f,nbins):
+	def Int(I,x):
+		return f(x)
+	fx = f(nbins)
+	diff_f = gradient(fx)#finds the derivative
+	integrate_f = integrate.odeint(Int,0.01,nbins)
+	values = [fx,diff_f,integrate_f]
+	return values
+
+
+
+
+
+def _plot(func):
+	x = np.arange(-6, 15, 0.05);
+	y = functions_plot(func,x)
+    	fx, = pyplot.plot(x, y[0], '-')
+ 	diff_f  = pyplot.plot(x, y[1], '-')
+	integral_f = pyplot.plot(x, y[2], '-')
+	pyplot.legend([fx,diff_f],['function','derivative'])
+    	pyplot.show()
+	
+	
+
+if __name__ == "__main__":
+	_plot(sine_and_cos)
+	
+
---
+++ make_fun_plots.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+import midpoint
+import calculus_plotter
+
+def plot_easy_fun():
+    calculus_plotter._plot(easy)
+
+def plot_exp_saturation(x):
+    calculus_plotter._plot(exp_saturation)
+
+def plot_exp_growth(x):
+    calculus_plotter._plot(exp_growth)
+
+def plot_sine_and_cos(t):
+    calculus_plotter._plot(sine_and_cos)
+
+def plot_sine_sq(t):
+    calculus_plotter._plot(sine_sq)
