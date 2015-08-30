f99c5065aa05c65d8ca80e346246cd38114084eb
fred-aims <fred@aims.edu.gh>
2015-08-30 21:33:00 +0000

    I corrected the errors in my midpoint.py script

--- midpoint.py
+++ midpoint.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
+import numpy as np
 
+def integrate(f,rangex):
+	accumulator = 0
+	for i in range(1,100,1):
+		Ii = (rangex[i] - rangex[i-1])*f((rangex[i-1] + rangex[i])/2.0)
+		accumulator = accumulator + Ii
+	return accumulator
+
+
+if __name__ == "__main__":
+	print (integrate(np.exp, np.linspace(0, 10, 100, endpoint=True)))
