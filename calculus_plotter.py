f320e18af59a94b148df374db40733ef2f668726
janayan <janyan@aims.edu.gh>
2015-08-30 10:18:13 +0000

    Fixing stuff

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -4,6 +4,7 @@ import math
 from pylab import *
 from scipy.misc import derivative
 import fun_plots as fp
+
 from scipy.integrate import quad
 import numpy as np
 
@@ -13,11 +14,11 @@ def fxn():
     return X
 
 def plotter(fxn):
-    
     figure()
     title("A graph of a function with it's derivative and integral")
     xlabel("X")
     ylabel("Y")
+
     xticks(arange(0,10,1))   
     #Ifxn= integrate.quad(fxn,X)
     plot(X,fxn(X), 'r', lw = 2, ms = 3, label = "f(x)")
@@ -25,5 +26,5 @@ def plotter(fxn):
     plot(X,Ifxn, 'g', lw = 2, ms = 3, label = "f'(x)")
     legend()
     
-plotter(fxn)
+
 
