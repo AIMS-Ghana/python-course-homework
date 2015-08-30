530d681670117d3d6290cbc00a06de0cb63e7756
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-08-30 11:52:56 +0000

    changes made to make fun plots file...

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -10,22 +10,6 @@ def plot_function(x):
 	calculus_plotter._plot(x)
 
 
-def plot_easy_fun():
-    calculus_plotter._plot(fun_plots.easy)
-
-def plot_exp_saturation():
-    calculus_plotter._plot(fun_plots.exp_saturation)
-
-def plot_exp_growth():
-    calculus_plotter._plot(fun_plots.exp_growth)
-
-def plot_sine_and_cos():
-    calculus_plotter._plot(fun_plots.sine_and_cos)
-
-def plot_sine_sq():
-    calculus_plotter._plot(fun_plots.sine_sq)
-
-
 if __name__ == "__main__":
 	for func in fun_plots.funclist:
 		plot_function(func)	
