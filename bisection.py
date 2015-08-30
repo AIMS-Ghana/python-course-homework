bddc30227738475dcc0a17954cb68d7b2187a001
Innocenter <innocenter@aims.edu.gh>
2015-08-30 19:59:46 +0000

    updating few more files that did not update earlier

--- bisection.py
+++ bisection.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 #Bisection method 
-
 def root(f, endpt, tol=0.001, N_max=100):
     a, b = endpt
     fa = f(a)
@@ -10,12 +9,12 @@ def root(f, endpt, tol=0.001, N_max=100):
     assert fa*fb < 0 
     midp = (a+b)/2
     fmidp = f(midp)
-    if (fmidp) < tol or (N_max== 0):
-        return midp
+    if (f(midp)) < tol or (N_max== 0):
+    	return midp
     elif fa*fmidp < 0:
-        return root(f,[a,midp])
+    	return root(f,[a, midp])
     else:
-	return root(f,[midp,b])
+   	return root(f,[midp,b])
 
 
 
