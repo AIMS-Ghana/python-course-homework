acb3d7c753dfb8715fa95e2ae54b0e62f4c3a9e9
Racheal-aims <racheal@aims.edu.gh>
2015-08-30 10:09:12 +0000

    Committing some files

--- midpoint.py
+++ midpoint.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
+
+
 def integrate(f, rangex):
     ss = 0
-    for i in range(len(q-1):
+    for i in range(len(rangex)-1):
         w = rangex[i+1] - rangex[i]
-        h = f((q[i+1] + q[i])/2)
+        h = f((rangex[i+1] + rangex[i])/2)
         ss = ss + w*h
     return ss
+
+
     # get range widths
     # get range midpoints
 
-if __name__ == "__main__":
-    print(integrate(lambda x:x, q:i))                  # first midpoint
+   #if __name__ == "__main__":
+    #main(sys.argv(1)) # first midpoint
     						# while(condition)loop: means repeat the condition over & over until it is satisfied
        						# for loop: use it when you have a no. of elements that u r going to use over and over
        						# e.g for i in range(5)
--- render_shapes.py
+++ render_shapes.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#!/usr/bin/python
-=======
 #!/usr/bin/env python3
->>>>>>> origin/master
 from draw_shape import draw
 
 draw("CIRCLE", 1000)
