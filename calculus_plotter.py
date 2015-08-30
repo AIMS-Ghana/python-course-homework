7b7336923d6d4ea93459dcdfd70f146cc84a8485
rebecca-aims <rfofie@aims.edu.gh>
2015-08-30 15:34:17 +0000

    updating assignment

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 import sys
 import pylab
-import numpy
+import numpy as np
 import fun_plots as fp
 from scipy.misc import derivative
+from scipy.integrate import odeint
 
-x = numpy.arange(0,10,0.01) # 100 linearly spaced numbers
-# compose plot
+x = np.arange(0,10,0.01) # 100 linearly spaced numbers
 
 def plotter(f):
-	pylab.figure()
-	df = derivative(f,x,dx=1,n=1)
-	
 	pylab.plot(x,f(x), 'b', label = "f(x)")
+        pylab.figure()
+	def g(I,x):
+		return f(x)
+
+	p = odeint(g,0,x)
+	df = derivative(f,x,dx=1,n=1)
+	pylab.plot(x,f(x), 'g')
 	pylab.plot(x,df,'r', label = "f'(x)")
+	pylab.plot(x,p,'c', label = "f'(x)")
 	pylab.legend()
 	pylab.show() # show the plot
 
 
+plotter(np.cos)
