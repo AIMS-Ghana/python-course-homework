93e6f4613b1d3fab4cdd77e637e2aee90ef1e308
janayan <janyan@aims.edu.gh>
2015-08-30 12:25:07 +0000

    submission of updated work

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,5 +1,7 @@
-import fun_plots as fp
-import calculus_plotter as cp
+#!/usr/bin/python
 
-for p in range(0,5):
-	cp.calculus_plotter(fp.funclist[p])
+import fun_plots
+import calculus_plotter
+
+for i in range(0,5):
+	calculus_plotter.plotter(fun_plots.funclist[i])
