7fb470220865eda674b839a86af78dfd0dd00974
kunda-aims <kunda@aims.edu.gh>
2015-08-31 00:24:32 +0000

    edited

--- bisection.py
+++ bisection.py
@@ -3,27 +3,31 @@ import sys
 import math
 #from math import error
 
-def f(x):
-	return x**3+3*x-5
+def g(x):
+	h = x**2+2*x-5
+	return h
 
-def root(f,rangex,tol = 1.0e-9): # find the root of a function using bisection method
+def root(h,rangex,tol = 1.0e-9): # find the root of a function using bisection method
 	a=rangex[0] 
 	b=rangex[1]
 	m = (a+b)*0.5
-	fa = f(a)
-	fb = f(b)
-	if fa == 0.0:
-		return m
-	if fb == 0.0:
-		return b
-	if fa*fb > 0.0:
-		print ('....root is not bracketed....')
-	while (rangex[1] - rangex[0]) < tol:
-		if f(m) < 0:
+	ha = h(a)
+	hb = h(b)
+	 
+	while (rangex[1] - rangex[0])*0.5 > tol:
+		if ha == 0.0:
+			return m
+		if hb == 0.0:
+			return b
+		if ha*hb > 0.0:
+			print ('....root is not bracketed....')
+		if ha*h(m) < 0:
+			return m
+		if g(m) < 0:
 			a = m 
-		elif f(m) > 0:
+		elif g(m) > 0:
 			b = m
-		elif f(m) == 0:
+		elif g(m) == 0:
 			return m
 		else:
 			pass
