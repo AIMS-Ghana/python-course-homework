d0655a311117d669573fd8db4079a8813c4de06f
nicholasopoku <nicholas@aims.edu.gh>
2015-08-31 23:44:04 +0000

    submiting my assignment on csv

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
 import fun_plots as fp
+import fun_plots2 as hp
 import calculus_plotter as cp
 
 for n in range(0,5):
-         cp.plotter(fp.funclist[n])
+       cp.plotter(fp.funclist[n])
+        
+         
+        
