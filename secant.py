83ffa0dda1da46ab0e093a0f4f44b54cd70e919d
bernard-aims <bernard@aims.edu.gh>
2015-08-31 16:57:10 +0000

    Committing again

---
+++ secant.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+def root(bisectf, rangex):
+    	area = 0.0
+   	for x in range(len(rangex)-1):
+		a =bisectf(rangex[x])
+		b =bisectf(rangex[x+1])
+		c =(rangex[x+1]-rangex[x]/(b-a))
+		d=(float(b*c))
+		area=rangex[x+1]-d
+
+	return area
+
+	
+
+               
+
+#if __name__ == '__main__':
+	
+
+
+
---
+++ trapezoid.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def integrate(intf, rangex):
+	area =0.0
+	for x0 in range(len(rangex)-1):
+		a = intf(rangex[x0])
+		b = intf(rangex[x0+1])
+		c = rangex[x0+1]-rangex[x0]
+		d = (a+b)*0.5
+		area = d*c
+	
+	return area
+	
+	
