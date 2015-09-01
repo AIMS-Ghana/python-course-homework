f9b0c554157dc3b12f312bc1fb2d8c142982389e
selom-aims <selom@aims.edu.gh>
2015-09-01 04:23:29 +0000

    submitting my assignment

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+<<<<<<< Updated upstream
 import sys
 import pylab
 import numpy as np
@@ -25,3 +26,25 @@ def plotter(f):
 
 plotter(np.cos)
 
+=======
+
+import sys
+import pylab
+import numpy
+import fun_plots as fp
+from scipy.misc import derivative
+
+x = numpy.arange(0,10,0.01) # 100 linearly spaced numbers
+# compose plot
+
+def plotter(f):
+	pylab.figure()
+	df = derivative(f,x,dx=1,n=1)
+	
+	pylab.plot(x,f(x), 'b', label = "f(x)")
+	pylab.plot(x,df,'r', label = "f'(x)")
+	pylab.legend()
+	pylab.show() # show the plot
+
+plotter(numpy.cos)
+>>>>>>> Stashed changes
