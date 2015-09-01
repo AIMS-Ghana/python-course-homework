0b20fec864c7b5e97fd8b74e1945553460bdca4b
eunice-aims <eunice@aims.edu.gh>
2015-09-01 00:45:43 +0000

    submitting my assignment

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -2,25 +2,27 @@
 import math
 import numpy as np
 import fun_plots as fp
+
 from matplotlib.pyplot import *
+
 from scipy.misc import derivative
 from scipy.integrate import odeint
 
 def plotter(fxn):
-    def t(I,X):
-        return fxn(X)
-    figure()
+    def t(I,x):
+        return fxn(x)
 
+    figure()
     title("A graph of a function with it's derivate and integral")
     xlabel("X")
     ylabel("Y")
-    xticks(arange(0,10,1))
-    X=arange(0,10,0.1)
-    Dfxn = derivate(fxn,X,dx=1,n=1)
-    Ifxn=odeint(t,0,X)
-    plot(X, fXn(X), 'b', lw = 2, label = "f(x)")
-    plot(X, Dfxn(X), 'r', lw = 2, label = "f(x)")
-    plot(X, Ifxn(X), 'bo', lw = 2, label = "integration")
+    #xtick(arange(0,10,1))
+    X=np.arange(0,10,0.1)
+    Dfxn = derivative(fxn,X,dx=1,n=1)
+    Ifxn = odeint(t,0,X)
+    plot(X, fxn(X), 'b', lw = 4, ms = 2, label = "f(x)")
+    plot(X, Dfxn, 'r', lw = 4, ms = 2, label = "f'(x)")
+    plot(X, Ifxn, 'b', lw = 4, ms = 2, label = "integration")
     legend(loc=0)
-    plt.show()
+    show()
 
---
+++ make_fun_plots.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import calculus_plotter
+import fun_plots
+
+
+if __name__ =="__main__":
+	calculus_plotter.plotter(fun_plots.easy)
