csvreader_basic.py added, not really what was intended I suspect, got into a tangle with key row being off by one using csv - go pandas, hard-wire in colums.....FIX (and learn some pandas)

calculus_plotter.py
@@ -36,12 +36,15 @@ def funcplot(f, *args):
 
       x1 = np.linspace(a1,b1,N-1)   # since np.diff puts values at N-1 midpoints             
       
-      name = f.__name__
+      name = f.__name__  # gets the name of a function
+
+      def h(I,x):       # this works if I don't use a lambda function, I is throwaway
+       return f(x)      # gets rid of the missing argument problem
 
       plt.plot(x,f(x),label=name)
       plt.plot(x1,np.diff(f(x))/np.diff(x),color='red',label = "d/dx("+name+")")
-      plt.plot(x,odeint(lambda y,x:f(x),fa,x),color='green', label = "int("+name+")")
-      
+ #     plt.plot(x,odeint(lambda I,x:f(x),fa,x),color='green', label = "int("+name+")") #notice the lambda function to insure there are TWO arguments for the rhs on odeint 
+      plt.plot(x,odeint(h,fa,x),color='green', label = "int("+name+")") # this works too with previously defined h
       plt.xlabel('x',style='italic')
       plt.ylabel("f(x), f'(x) and int f(x)",style = 'italic')
       plt.legend(loc='best')