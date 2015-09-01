35aad13b1b74ae99f569d3c7bab080f8e0c7c6dd
fred-aims <fred@aims.edu.gh>
2015-09-01 00:43:42 +0000

    I updated my make_fun_plots, fun_plots 1and 2 and turnning in the loan_data script

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -7,16 +7,16 @@ from scipy.integrate import odeint
 #import fun_plots as fp
 #from simpy import integrate
 
-funames = [r'$f(x) = x$' , r'$f(x) = 1-e^{-x}$',r'$f(x) = e^{5x}$',r'$f(x) = \sin(t) + \cos(t)$',r'$f(x) = \sin^{2}(t)$']
 
 def plotter(fxn,i):
+    funames = [r'$f(x) = x$' , r'$f(x) = 1-e^{-x}$',r'$f(x) = e^{5x}$',r'$f(x) = \sin(t) + \cos(t)$',r'$f(x) = \sin^{2}(t)$']
     figure(i+1),grid(True)
     title(" A graph of {0} with it's derivative and integral".format(funames[i]))
     xlabel("X")
     ylabel(r'$f(x) , f\prime(x) , \int{f(x)}dx$')
-    xticks(arange(0,10,1))
+    xticks(arange(-5,5,1))
 
-    X = arange(0,10,0.01)
+    X = arange(-5,5,0.01)
 
     def k(I,X):
 	return fxn(X)
--- make_fun_plots.py
+++ make_fun_plots.py
@@ -2,7 +2,11 @@
 
 import calculus_plotter
 import fun_plots
+import fun_plots2
 
 
 for i in range(0,5):
     calculus_plotter.plotter(fun_plots.funclist[i],i)
+
+for i in range(0,5):
+    calculus_plotter.plotter(fun_plots2.funclist[i],i)
