66bb2ee03ef68c866e6c0f6940070a8cbef8fb0e
emascuryo <sowusu@aims.edu.gh>
2015-09-01 12:02:02 +0000

    turning in the files about donuts and loans

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import fun_plots as fplt
+import fun_plots2 as fp
 import calculus_plotter as cplt
 
 for k in range(0,5):
 	cplt.calculus_plotter(fplt.funclist[k])
+	cplt.calculus_plotter(fp.funclist[k])
