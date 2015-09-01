39114b01ae5c0eb9e151085af60f9fafd7a5a020
Des Johnston <des@aims.edu.gh>
2015-09-01 10:00:46 +0000

    Copied from teacher, own attempt mangled iterating through dictionaries....

--- jsonreader_basic.py
+++ jsonreader_basic.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
 
 import json
-from collections import defaultdict
 
-df  = json.load(open('donuts.json'))
+myfile = "donuts.json"
 
+for base_donut in json.load(open(myfile)):
 
+  base_name = base_donut["name"]
 
+  for batter in base_donut["batters"]["batter"]:
 
-#print(df)
+        batter_name = batter["type"]
 
+        for topping in base_donut["topping"]:
 
+              topping_name = topping["type"]
 
-print(type(df))
-
-for doc in df:
-   for key, value in doc.iteritems():
-          print key, value
-
+              print(base_name+","+batter_name+","+topping_name)
 
 
 
