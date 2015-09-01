9c85a77a08989965969753ebfdf786d3380d5574
nicholasopoku <nicholas@aims.edu.gh>
2015-09-01 11:20:42 +0000

    edited assignment

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-
 import fun_plots as fp
-import fun_plots2 as hp
 import calculus_plotter as cp
+import fun_plots2 as fp2
 
 for n in range(0,5):
        cp.plotter(fp.funclist[n])
+
+for n in range(0,5):
+     	cp.plotter(fp2.funclist1[n])
         
          
         
