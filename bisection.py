a1dd44c8eed824732fd8a2e05f9c7fab65995783
amadou-aims <amadou@aims.edu.gh>
2015-09-02 21:58:53 +0000

    type anything

--- bisection.py
+++ bisection.py
@@ -6,9 +6,6 @@ def root(f, x):
 	def bisect(a,b,tol):
 		tol = 0.001
 		c = (a+b)/2.0
-#		for n in range(len(b)-1):
-#			c = (b[n] + b[n+1])/2.0
-#			d = a(c)
 		while (b-a)/2.0 > tol:
 			if f(c) == 0:
 				return c
@@ -17,9 +14,7 @@ def root(f, x):
 			else:
 				a = c
 		return c
-
-if __name__  =="__main__":
-
+if __name__ == "__main__":
 	main(sys.argv[1:])
 	print 'The root is: '
-	print bisection(int((sys.argv[1]),int(sys.argv[2]),float(sys.argv[3]))
+	print bisection(int(sys.argv[1]),int(sys.argv[2]),float(sys.argv[3]))
