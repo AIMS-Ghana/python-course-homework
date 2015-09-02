989b49914233593bcdc9231ce244485089072c96
DIDIER-AIMS <didier@aims.edu.gh>
2015-09-02 06:46:29 +0000

    my assignment

--- random_pi.py
+++ random_pi.py
@@ -1,10 +1,54 @@
-#!/usr/bin/puyhon
-import sys
-import numpy.random as npr
-from numpy import reshape
+#!/usr/bin/python
 
-seed = int(sys.argv[1])
-npr.seed(seed)
+
+import random
+import sys
+import numpy as np
+
 n = int(sys.argv[2])
-samples = npr.random_sample(n)
-circ = reshape(samples, ())
+seed = int(sys.argv[1])
+
+
+def circle_area(seed, n):
+	random_points = []
+	for i in np.arange(0, n+1):
+		x = random.random()
+		y = random.random()
+		a = (x,y)
+		random_points.append(a)
+
+	#print random_points
+	count = 0.0
+	for j in random_points:
+		if (j[0])**2 + (j[-1])**2 <= 1:
+			count = count + 1
+		else:
+			pass
+	print count
+	pi = (count/n)*4
+	print "circle area pi :", pi
+
+
+def sphere_volume(seed, n):
+	random_points = []
+	for i in np.arange(0, n+1):
+		x = random.random()
+		y = random.random()
+		z = random.random()
+		a = (x,y,z)
+		random_points.append(a)
+
+	#print random_points
+	count = 0.0
+	for j in random_points:
+		if (j[0])**2 + (j[1])**2 + (j[-1])**2 <= 1:
+			count = count + 1
+		else:
+			pass
+	print count
+	pi = (count/n)*6
+	print "sphere volume pi :", pi
+
+circle_area(seed, n)
+sphere_volume(seed, n)
+
