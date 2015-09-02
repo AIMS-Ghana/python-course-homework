9228c7a107af5977c018f74d7ee0b01496240a26
eugene_aims <eugene@aims.edu.gh>
2015-09-02 14:06:41 +0000

    rewrote circle using dictionaries

--- circle.py
+++ circle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys
 import math
-greeting="area= {}, perimeter = {}"
+#greeting="area= {}, perimeter = {}"
+vol= {'word' : "area" , 'word1' : "perimeter"}
 
 num1 = sys.argv[1]
 # Add two numbers
@@ -9,5 +10,5 @@ UncleSam = 2*math.pi*(float(num1))
 product =math.pi*(float(num1))**2
 
 # Display the sum
-print(greeting.format(num1, UncleSam))
-
+#print(greeting.format(num1, UncleSam))
+print vol['word']," = ", num1 , vol['word1'],"= {1} ".format(num1 , UncleSam )
