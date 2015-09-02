4d96cc9f38dc706fe9d625c898f4c481aef5d57e
Innocenter <innocenter@aims.edu.gh>
2015-09-02 01:17:19 +0000

    Random pi code

---
+++ random_pi.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import random  
+import sys
+from numpy import sqrt
+#calculating Pi using Monte Carlo Method
+
+seed=int(sys.argv[1]) 
+n=int(sys.argv[2])  
+count_inside=0
+random.seed(seed)
+
+#def pi_circlearea():
+for i in range(n):  
+	x=random.random()  
+	y=random.random()
+	
+        if sqrt(x*x+y*y)<=1:              
+		count_inside+=1
+	else:
+		pass 
+	pi_circlemethod=4.0*count_inside/n  
+print "The circle-area method pi:" , pi_circlemethod
+
+#def p_spherevolume():
+
+# using sphere-volume method to find pi
+c_inside=0
+for i in range(n):  
+	x=random.random()  
+	y=random.random()
+	z=random.random()
+	
+        if sqrt(x*x+y*y+z*z)<=1:              
+		c_inside+=1
+	else:
+		pass 
+	pi_spheremethod=6.0*c_inside/n 
+
+print "The sphere-volume method pi:" , pi_spheremethod
+
+
+
+
+
+ 
