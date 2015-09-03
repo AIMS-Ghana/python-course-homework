769ac6a488d6514d9eed6ef6f8fde41edbd3318c
rhodarlin-aims <rhodarlin@aims.edu.gh>
2015-09-03 15:21:27 +0000

    made changes

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import calculus_plotter as cp
-import fun_plots as fp
 
+import fun_plots2 as fp2
 for i in range(0,5):
-	cp.rhodalin(fp.funclist[i])
+	cp.rhodalin(fp2.funclist[i])
