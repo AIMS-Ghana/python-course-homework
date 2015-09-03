7268ff8d1a42c8db735f36c34375980975480afe
eugene_aims <eugene@aims.edu.gh>
2015-09-03 16:36:50 +0000

    tried assignment on the shape objects

--- shape_objects.py
+++ shape_objects.py
@@ -28,16 +28,119 @@ class Shape:
         pass
 
 class Triangle(Shape):
-    pass
+    def kind(self):
+        return "Triangle"
+
+
+
+    def invert_area(self, area):
+       return 2*(area/3**(0.5))**(0.5)
+
+
+
+
+
+    def dimname(self):
+        return "Side"
+
+
+
+
+
+    def __init__(self, area):
+        self.__area = area
+        self.__dim = self.invert_area(area)
+
+
+    strformat = "{}, area: {}, {}: {}"
+
+    def __str__(self):
+        return Shape.strformat.format(
+          self.kind(),
+          self.__area,
+          self.dimname(),
+          self.__dim
+        )
+
+
+
+
 
 class Square(Shape):
-    pass
+    def kind(self):
+        return "Square"
+
+
+
+
+
+    def invert_area(self, area):
+        return area**(0.5)
+
+    def dimname(self):
+        return "Side"
+
+    def __init__(self, area):
+        self.__area = area
+        self.__dim = self.invert_area(area)
+
+    strformat = "{}, area: {}, {}: {}"
+
+    def __str__(self):
+        return Shape.strformat.format(
+         self.kind(),
+         self.__area,
+         self.dimname(),
+         self.__dim
+       )
+
+
+
 
 from math import pi
 
 class Circle(Shape):
-    pass
+	def kind(self):
+        	return "Circle"
+ 	
+	
+	def invert_area(self, area):
+        	return (area/pi)**(0.5)
 
+
+	def dimname(self):
+	        return "Radius"
+
+	def __init__(self, area):
+        	self.__area = area
+        	self.__dim = self.invert_area(area)
+
+	strformat = "{}, area: {}, {}: {}"
+  
+
+
+	def __str__(self):
+	        return Shape.strformat.format(
+          	self.kind(),
+          	self.__area,
+          	self.dimname(),
+          	self.__dim
+        	)
+    
 if __name__ == "__main__":
+    
+    import sys
+    #a = sys.argv[0]
+    #b = sys.argv[1]
+    #c = sys.argv[2]
+
+
+
+
+
     circ = Circle(10)
     print(circ)
+    tria = Triangle(10)
+    print(tria)
+    squar = Square(10)
+    print(squar)
