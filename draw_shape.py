c7b7ea9ae398e0fe0ab1f6fc3bd3294206b39474
nganza <dative@aims.edu.gh>
2015-09-03 13:57:22 +0000

    modified midpoint.py

--- draw_shape.py
+++ draw_shape.py
@@ -1,70 +1,42 @@
-#!usr/local/bin/python3
+#!/usr/bin/python3
 import turtle
-import shape
-import math
 import sys
+import shapes
 
-
-def draw_shape():
-    turtle.bdcalor("red")
-    turtle= turtle.turtle()
-    turtle.shape("turtle")
-    turtle.color("yellow")
-    turtle.speed(2)
-    turtle.exitonclick()
-
-def CIRCLE(radius):
+def circle(radius):
     turtle.circle(radius)
 
-def n_side(n, side):
-    turtle.forwad(side)
-    turtle.right(30)
+def n_sides(n, side):
+    turn = 180 - (n-2)*180.0/n
+    for i in range(n-1):
+        turtle.forward(side)
+        turtle.right(turn)
     turtle.forward(side)
-    tuutle.exitonclick()
-draw_CIRCLE()
 
-def TRIANGLE(side):
-    n_side(3,side)
-    turtle.right(120)
-    turtle.forwad(side)
-    turtle.right(120)
-    turtle.forwad(side)
-    turtle.right(120)
-    turtle.forwad(side)
-    turtle.exitonclick()
-draw_TRIANGLE()
+def triangle(side):
+    n_sides(3,side)
 
-def SQUARE(side):
-    ngon(4,side)
-    turtle.right(90)
-    turtle.forwad(l)
-    turtle.right(90)
-    turtle.forwad(l)
-    turtle.right(90)
-    turtle.forwad(l)
-    turtle.right(90)
-    turtle.forwad(l)
-    turtle.exitonclick()
-draw_SQUARE()
+def square(side):
+    n_sides(4,side)
+
 figs = {
     'CIRCLE':circle,
     'SQUARE':square,
     'TRIANGLE':triangle
 }
 
-def draw(shape, area):
-    dim = compute(shape, area)
+def draw(shape, area, fill="blue", cont=False):
+    dim = calc_shape(shape, area)
+    turtle.color(fill)
+    turtle.begin_fill()
     figs[shape](dim)
-    turtle.exitonclick()
+    turtle.end_fill()
+    if not cont:
+        turtle.exitonclick()
 
-
-import sys 
+import sys
 
 if __name__ == "__main__":
     shape = sys.argv[1]
     area = float(sys.argv[2])
     draw(shape, area)
-
-
-	
-
