474b99329cde20bc305b14e524d8dfa525e10d51
janayan <janyan@aims.edu.gh>
2015-09-03 20:41:47 +0000

    Submission pf assignment

--- shape_objects.py
+++ shape_objects.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
+import math 
+
+
 class Shape:
     def kind(self):
-        return "Shape"
+        return "shape"
 
     def invert_area(self, area):
-        return area
+      return area
 
     def dimname(self):
         return "HUH?"
-
+	
     def __init__(self, area):
         self.__area = area
         self.__dim = self.invert_area(area)
@@ -24,20 +27,44 @@ class Shape:
           self.__dim
         )
 
-    def draw(turt):
+    def draw(turt): #drawing the shapes
         pass
 
 class Triangle(Shape):
-    pass
+    def kind(self):
+        return "Triangle"
+    def invert_area(self, area):
+      return math.sqrt((float(area)*4)/math.sqrt(3))
+    def dimname(self):
+        return "Side"
+
 
 class Square(Shape):
-    pass
+    def kind(self):
+        return "Square"
+    def invert_area(self, area):
+      return float(area**0.5)
+    def dimname(self):
+        return "Side"
+ 
 
 from math import pi
 
 class Circle(Shape):
-    pass
+    def kind(self):
+        return "Circle"
+    def invert_area(self, area):
+      return math.sqrt(float(area)/math.pi)
+    def dimname(self):
+        return "Radius"  
+
 
 if __name__ == "__main__":
     circ = Circle(10)
+    tri= Triangle(5)
+    sq=Square(5)
     print(circ)
+    print (tri)
+    print (sq)
+
+
