0bfec4c3447ba07b2dcfd4d45610569db2ef426f
Des Johnston <des@aims.edu.gh>
2015-09-03 21:50:55 +0000

    ..turtles working..

--- shape_objects.py
+++ shape_objects.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 class Shape:
+
+    
+
     def kind(self):
         return "Shape"
 
@@ -11,8 +14,8 @@ class Shape:
         return "HUH?"
 
     def __init__(self, area):
-        self.__area = area
-        self.__dim = self.invert_area(area)
+        self._area = area
+        self._dim = self.invert_area(area)
 
     
     strformat = "{}, area: {}, {}: {}"
@@ -20,14 +23,13 @@ class Shape:
     def __str__(self):
         return Shape.strformat.format(
           self.kind(),
-          self.__area,
+          self._area,
           self.dimname(),
-          self.__dim
+          self._dim
         )
 
-    def draw(turt):
+    def draw(self,turt):
         pass
-
 #####################################################################################
 
 class Triangle(Shape):
@@ -36,10 +38,24 @@ class Triangle(Shape):
      
      def invert_area(self, area):
         return (4*area/3**(1/2.0))**(1/2.0)
-
+    
      def dimname(self):
-        return "2d"
-       
+        return "L"
+     
+     def draw(self,turt):
+        import turtle
+        wn = turtle.Screen()
+        L= self._dim
+        scale = 30.0
+        Lshow = L*scale
+        wn = turtle.Screen()
+        turtle.forward(Lshow)
+        turtle.left(120)
+        turtle.forward(Lshow)
+        turtle.left(120)
+        turtle.forward(Lshow)
+        turtle.write("Click Me",font=("Arial", 20, "normal"))
+        wn.exitonclick()
 
 class Square(Shape):
      def kind(self):
@@ -49,9 +65,24 @@ class Square(Shape):
         return area**(1/2.0)
 
      def dimname(self):
-        return "2d"
-       
-       
+        return "L"
+     
+     def draw(self,turt): 
+       import turtle
+       wn = turtle.Screen() 
+       L = self._dim
+       scale = 30.0
+       Lshow = L*scale
+       wn = turtle.Screen()
+       turtle.forward(Lshow)
+       turtle.left(90)
+       turtle.forward(Lshow)
+       turtle.left(90)
+       turtle.forward(Lshow)
+       turtle.left(90)
+       turtle.forward(Lshow)
+       turtle.write("Click Me",font=("Arial", 20, "normal"))
+       wn.exitonclick()  
        
 
 from math import pi
@@ -64,19 +95,32 @@ class Circle(Shape):
         return (area/pi)**(1/2.0)
 
      def dimname(self):
-        return "2d"
+        return "r"
      
-         
+     def draw(self,turt): 
+       import turtle
+       wn = turtle.Screen() 
+       scale = 30.0
+       r = self._dim
+       rshow = r*scale
+       wn = turtle.Screen()
+       turtle.circle(rshow)
+       turtle.write("Click Me",font=("Arial", 20, "normal"))
+       wn.exitonclick()   
        
       
 
 #############################################################
 
 if __name__ == "__main__":
-    circ = Circle(10)
+    circle = Circle(10)
     triangle = Triangle(4)
     square = Square(4)
 
-    print(circ)
+    print(circle)
     print(triangle)
     print(square)
+
+    circle.draw(circle)
+    triangle.draw(triangle)
+    square.draw(square)
