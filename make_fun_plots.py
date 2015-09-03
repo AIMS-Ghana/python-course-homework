06da8fe13d8d563e36836cde119e460752de8139
Stephen K Amponsah <samponsah@aims.edu.gh>
2015-09-03 13:57:54 +0000

    updates on .md file

--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import fun_plots
+import fun_plots2
 import calculus_plotter
 
 
@@ -10,5 +11,7 @@ def plot_function(x):
 
 if __name__ == "__main__":
 	for func in fun_plots.funclist:
-		plot_function(func)	
+		plot_function(func)
+	for func2 in fun_plots2.funclist:
+		plot_function(func2)	
 	
---
+++ skamponsah.md
@@ -0,0 +1,3 @@
+AIMS-Ghana Python course work
+
+Changes made to make_fun_plots.py and fun_plots2.py added as part of home work on 2nd September
