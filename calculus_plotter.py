85bfb3d3ba499bb6d5f0eba581e526e32700af4c
Bedetse <beni@aims.edu.gh>
2015-09-03 15:37:13 +0000

    fixing merge issues

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -2,6 +2,7 @@
 from numpy import gradient
 from scipy.integrate import odeint
 
+
 def sine_and_cos(t):
     return np.sin(t) + np.cos(t)
 
@@ -11,7 +12,7 @@ def calc_all(h, rangex, ifxmin = 0):
     return {
         'h'  : hx,
         'dh' : gradient(hx),
-        'Ih' : odeint(lambda y, x:h(x), 0, rangex)
+        'Ih' : odeint(lambda y, x:h(x), 0, rangex)s
     }
 
 import matplotlib.pyplot as pyplot
@@ -28,4 +29,5 @@ if __name__ == "__main__":
     from numpy import linspace
     x = linspace(-10,15,25,endpoint=True)
     plot_all(lambda x:x, x)
+    plot_all(Ih, x)
     plot_all(Ih, x)
