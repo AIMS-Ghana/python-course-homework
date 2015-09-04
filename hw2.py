58043ca7977fcf05259796a051b86bc05bdc2cbd
rebecca-aims <rfofie@aims.edu.gh>
2015-09-04 10:21:21 +0000

    updating assignment

--- hw2.py
+++ hw2.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python
+
 import sys
-greeting="hello,{0}!"
-word= ""
-if len (sys.argv) ==1
-word="word"
-elif len(sys.argv)==2:
 
-word=sys.argv[1]
-else:
-world= ", ".join(sys.arg[1:-1]) + " and" + sys.argv[-1]
-print(greeting.format(world))
-
+print("hello, "+sys.argv[1]+"!")
