c9f26b87f7667beaadd717096e24a7ae3dc4176d
eunice-aims <eunice@aims.edu.gh>
2015-09-04 03:08:47 +0000

    submit

--- shape_objects.py
+++ shape_objects.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python3
+import sys
 
-class Shape:
-    def kind(self):
-        return "Shape"
-
-    def invert_area(self, area):
-        return area
-
-    def dimname(self):
-        return "HUH?"
-
-    def __init__(self, area):
-        self.__area = area
-        self.__dim = self.invert_area(area)
-
-    strformat = "{}, area: {}, {}: {}"
-
-    def __str__(self):
-        return Shape.strformat.format(
-          self.kind(),
-          self.__area,
-          self.dimname(),
-          self.__dim
-        )
-
-    def draw(turt):
-        pass
-
-class Triangle(Shape):
-    pass
-
-class Square(Shape):
-    pass
 
 from math import pi
 
+
+class Shape:
+    def draw(turtle):
+        pass
+
+class Triangle(Shape):
+        pass
+
+class Square(Shape):
+        pass
+
+
+
 class Circle(Shape):
-    pass
+        pass
+
+def invert_area(self, area):
+    return area
+
+class bar(circle):
+    def __init__(self,dimnames):
+
+    #self.dd = {n:i for i,n in enumerate(dimnames)}
+   # def dimname(self):
+     #   return "HUH?"
+
+strformat = "{kind}, area: {area}, {dimname}: {dim}"
+
+def __str__(strformat):
+    return Shape.strformat.format(
+        self.kind(),
+        self.__area(),
+        self.dimname(),
+        self.__dim
+        )
+
+
 
 if __name__ == "__main__":
     circ = Circle(10)
