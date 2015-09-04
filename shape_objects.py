83b79338fd7f38841cbcf8c73bdba18cd50e751f
sarobidy19-aims <andriahermanana@aims.edu.gh>
2015-09-04 00:59:26 +0000

    shape_objects

--- shape_objects.py
+++ shape_objects.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python
+from math import *
 class Shape:
     def kind(self):
         return "Shape"
@@ -27,17 +27,107 @@ class Shape:
     def draw(turt):
         pass
 
-class Triangle(Shape):
-    pass
+class Triangle:
+	def kind(self):
+    	    return "Triangle"
+	def invert_area(self, area):
+	        return (sqrt((area*4)/sqrt(3)))
+	def dimname(self):
+	        return "side"
+	def __init__(self, area):
+        	self.area = area
+        	self.dim = self.invert_area(area)
 
-class Square(Shape):
-    pass
+        strformat = "{}, area: {}, {}: {}"
 
-from math import pi
+	def __str__(self):
+    	    return Shape.strformat.format(
+    	      self.kind(),
+    	      self.area,
+    	      self.dimname(),
+    	      self.dim
+    	    )
+	def draw(self,turt):
+        	import turtle
+        	l = self.dim
+        	turtle.forward(l)
+		turtle.left(120)
+		turtle.forward(l)
+		turtle.left(120)
+		turtle.forward(l)
+        	turtle.exitonclick()
 
-class Circle(Shape):
-    pass
+class Square:
+	def kind(self):
+		return "Square"
+    	def invert_area(self, area):
+        	return (sqrt(area))
+	def dimname(self):
+        	return "side"
+
+	def __init__(self, area):
+    		self.area = area
+    		self.dim = self.invert_area(area)
+
+	strformat = "{}, area: {}, {}: {}"
+	def __str__(self):
+	        return Shape.strformat.format(
+	          self.kind(),
+	          self.area,
+	          self.dimname(),
+	          self.dim
+	        )
+	def draw(self,turt):
+        	import turtle
+        	l = self.dim
+        	turtle.forward(l)
+		turtle.left(90)
+		turtle.forward(l)
+		turtle.left(90)
+		turtle.forward(l)
+		turtle.left(90)
+		turtle.forward(l)
+        	turtle.exitonclick()
+
+class Circle:
+   
+    def kind(self):
+        return "circle"
+
+    def invert_area(self, area):
+        return sqrt (area / pi)
+
+    def dimname(self):
+        return "radius"
+
+    def __init__(self, area):
+        self.area = area
+        self.dim = self.invert_area(area)
+
+    strformat = "{}, area: {}, {}: {}"
+
+    def __str__(self):
+        return Circle.strformat.format(
+          self.kind(),
+          self.area,
+          self.dimname(),
+          self.dim
+        )
+
+    def draw(self,turt):
+        import turtle
+        l = self.dim
+        turtle.circle(l)
+        turtle.exitonclick()
+
 
 if __name__ == "__main__":
-    circ = Circle(10)
-    print(circ)
+	obj = Triangle(10000)
+    	print (obj)
+    	obj.draw(obj)
+	obj = Square(10000)
+    	print (obj)
+    	obj.draw(obj)
+	obj = Circle(10000)
+    	print (obj)
+    	obj.draw(obj)
