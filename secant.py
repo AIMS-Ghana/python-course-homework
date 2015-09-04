11ad4033d11dd58d4617a75028cbd60bec9a37f8
nabnel <mike@aims.edu.gh>
2015-09-04 14:43:48 +0000

    Adding updates for midpoint, bisection, trapezoid and secant

--- secant.py
+++ secant.py
@@ -5,9 +5,9 @@ def xn(f, x1, x2):
     return x2 - f(x2) * (x2 - x1) / ( f(x2) - f(x1) )
 
 
-def root(f, rangex):
+def root(f, guess):
+    (x1, x2) = guess
     tol = 10**(-8)
-    x1, x2 = rangex[0], rangex[ len(rangex) - 1]
     x3 = xn(f, x1, x2)
     while abs(f(x3)) >= tol:
         print(x1,x2,x3)
