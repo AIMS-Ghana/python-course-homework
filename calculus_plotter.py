57e441480ddc9438b9a85f345643a37c35763183
kunda-aims <kunda@aims.edu.gh>
2015-09-04 11:16:25 +0000

    upgrading files

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -12,15 +12,25 @@ def plotter(f):
 	def h(I,x):
 		return f(x)
 	init = 0
-	x = np.arange(-10.0, 20.0, 0.01) # range of x values on graph
+	func_names = [r'$f(x) = x$',r'$f(x) = 1 - e^{-x}$', r'$f(x) = e^{5x}$',r'$f(t) = \sin(t) + \cos(t)$',r'$f(t) = \sin^{2}(t)$']
+	for i in range (0,5):
+		funclist=(i+1)
+	#fig = plt.figure() 
+	#fig.suptitle("Graph of {0} together with its derivative and integral",format.(func_names))
+	fig = plt.figure()
+	#plt.plot(data)
+	fig.suptitle('graph of {}'.format.func_names(i),'with its derivative and integral')
+	x = np.arange(-30.0, 30.0, 0.0001) # range of x values on graph
 	y,dydx,i = func(f,x),deriv(f,x),odeint(h,init,x)  # define the y values on graph
-	plt.plot(x,y,'r', label='f(x)') # plot x against y
-	plt.plot(x,dydx,'g', label= 'dydx')
-	plt.plot(x,i,'b', label = 'integral')
+	plt.plot(x,y,'r', lw = 2, label='f(x)') # plot x against y
+	plt.plot(x,dydx,'g', lw = 2, label= 'dydx')
+	plt.plot(x,i,'b', lw = 2, label = r'$\int{f(x)}dx$')
 	plt.xlabel('x-axis') # label x-axis
 	plt.ylabel('y-axis') # label y-axis
 	plt.ylim(-5,5)
 	plt.legend()
+	mng = plt.get_current_fig_manager()
+	mng.frame.Maximize(True)
 	plt.show() # show graph
 
 
--- make_fun_plots.py
+++ make_fun_plots.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import fun_plots as fp
+import fun_plots2 as fp2
 import calculus_plotter as cp
 
 for i in range (0,5):
 	cp.plotter(fp.funclist[i])
- 
+ 	cp.plotter(fp2.funclist[i])
