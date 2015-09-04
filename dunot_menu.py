30aeaecdc98bf0ea89ae6294f6576b074378a910
Grace-aims <glorie@aims.edu.gh>
2015-09-04 10:17:01 +0000

    I send you my homework

---
+++ dunot_menu.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import json
+
+myfile = "donuts.json"
+
+#for base_donut in json.load(load(open(myfile))):
+for base_donut in json.load(open(myfile)):
+	base_name = base_donut["name"]
+        for batter in base_donut["batters"]["batter"]:
+	    batter_name = batter["type"]
+            for topping in base_donut["topping"]:
+	        topping_name = topping["type"]
+		print(base_name+", "+batter_name+","+topping_name)
+
+
+
