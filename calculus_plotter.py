e26edf60166a0e1b14a75a76a309f1223fa22c80
kunda-aims <kunda@aims.edu.gh>
2015-09-04 11:59:26 +0000

    modified

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -12,16 +12,18 @@ def plotter(f):
 	def h(I,x):
 		return f(x)
 	init = 0
-	func_names = [r'$f(x) = x$',r'$f(x) = 1 - e^{-x}$', r'$f(x) = e^{5x}$',r'$f(t) = \sin(t) + \cos(t)$',r'$f(t) = \sin^{2}(t)$']
-	for i in range (0,5):
-		funclist=(i+1)
-	#fig = plt.figure() 
-	#fig.suptitle("Graph of {0} together with its derivative and integral",format.(func_names))
+	#def graph_name(func_names):
+	#func_names = [r'$f(x) = x$',r'$f(x) = 1 - e^{-x}$', r'$f(x) = e^{5x}$',r'$f(t) = \sin(t) + \cos(t)$',
+	#r'$f(t) = \sin^{2}(t)$']
+	#for i in range (0,5):
+		#for func_name in func_names:
+			#func_name = (i+1)
+	
+		#return func_name 
 	fig = plt.figure()
-	#plt.plot(data)
-	fig.suptitle('graph of {}'.format.func_names(i),'with its derivative and integral')
-	x = np.arange(-30.0, 30.0, 0.0001) # range of x values on graph
-	y,dydx,i = func(f,x),deriv(f,x),odeint(h,init,x)  # define the y values on graph
+	fig.suptitle('graph of f(x),its derivative and integral') 
+	x = np.arange(-30.0, 30.0, 0.01) # range of x values on graph
+	y,dydx,i = func(f,x),deriv(f,x),odeint(h,init,x)   # define the y values on graph
 	plt.plot(x,y,'r', lw = 2, label='f(x)') # plot x against y
 	plt.plot(x,dydx,'g', lw = 2, label= 'dydx')
 	plt.plot(x,i,'b', lw = 2, label = r'$\int{f(x)}dx$')
@@ -30,7 +32,7 @@ def plotter(f):
 	plt.ylim(-5,5)
 	plt.legend()
 	mng = plt.get_current_fig_manager()
-	mng.frame.Maximize(True)
+	#mng.frame.Maximize(True)
 	plt.show() # show graph
 
 
