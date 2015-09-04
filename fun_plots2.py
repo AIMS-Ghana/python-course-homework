16344eff0ce12d37dd060a56efd31b5f4a48246c
nicholasopoku <nicholas@aims.edu.gh>
2015-09-04 12:41:06 +0000

    submit

--- fun_plots2.py
+++ fun_plots2.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 
 import numpy as np
+import calculus_plotter as cp
 
-def sin(x):
-    return (x)
+
+def sin2(x):
+	return np.sin(2*x)
+
+def square(x):
+	return (x**2)
+
+def x_exp(x):
+	return (x**2)*np.exp(x)
 
 def cos(x):
-    return (x)
+	return np.cos(x)
 
+def sin(x):
+	return np.sin(x)
 
+funclist1 = [sin2, square, x_exp, cos, sin]
 
-funclist1 = [
-    sin(x), cos(x)
-]
+if __name__ == "__main__":
+	for n in np.arange(0,5):
+		cp.plotter(funclist1[n])
