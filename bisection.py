84fe2a9e89d3b679b5a306da63ca0fa554a74188
rebecca-aims <rfofie@aims.edu.gh>
2015-09-06 16:37:15 +0000

    please I have corrected my codes

--- bisection.py
+++ bisection.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python
+
 import sys
-import math
 
-'''def f(x):
-	return x**3 + x -1
-'''	
-def root(f, V, tol=0.0000001, max_n=100):
-	a = V [0]
-	b = V [1]
-	#if f(c) == 0.0:
-		#return c
-	while abs (b-a) > tol:
-		c = float (a+b) / 2
-		if f(a)*f(c) < 0:
-			b = c
-		if f(b)*f(c) < 0:
-			a = c
-	return a
 	
-def main(argv):
-	if (len(sys.argv) != 4):
-		sys.exit('Usage: bisection.py <a> <b> <tol>')
-	
-	print 'The root is: ',
-	print bisection(int(sys.argv[1]),int(sys.argv[2]),float(sys.argv[3]))
-
-if __name__ == "__main__":
-	main(sys.argv[1:])
-
-
-
-
-
+def root(f,rangex, tol=0.005):
+	a=rangex[0]
+	b=rangex[1]
+	c = (a+b)*0.5
+	assert (b-a)*0.5 > tol
+	if f(c) == 0:
+		return c
+	elif f(a)*f(c) < 0:
+		b = c
+	else :
+		a = c
+	return c
--- draw_shape.py
+++ draw_shape.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python
 import turtle
 
-from shapes import compute  
+from shapes import compute
 
-def circle(radius):
-    turtle.circle (radius):
+def circle(a):
+    turtle.circle(a)
 
-def ngon(n,side):
-    turn= 180-(n-2)*180.0/n
-    for i in range (n-1)
-    turtle.forward(side)
-    turtle.right(turn)
+def ngon(n, side):
+    turn = 180 - (n-2)*180.0/n
+    for i in range(n-1):
+        turtle.forward(side)
+        turtle.right(turn)
     turtle.forward(side)
 
 def triangle(side):
     ngon(3,side)
 
-def  square 
-    ngon (4,side)
+def square(side):
+    ngon(4,side)
 
+figs = {
+    'CIRCLE':circle,
+    'SQUARE':square,
+    'TRIANGLE':triangle
+}
 
-figs={
-'CIRCLE':circle,
-'SQUARE':square,
-'TRIANGLE':triangle
-
-def draw(shape,area):
-    dim = compute (shape,area):
+def draw(shape, area, fill="black", cont=False):
+    dim = compute(shape, area)
+    turtle.color(fill)
+    turtle.begin_fill()
     figs[shape](dim)
-    turtle.exitonc check()
+    turtle.end_fill()
 
 import sys
-       if_name_="main_":
-       shape=sys.argv[1]
-       area=float(sys.argv[2])
-       draw(shape,area)
 
+if __name__ == "__main__":
+    shape = sys.argv[1]
+    area = float(sys.argv[2])
+    draw(shape, area)
