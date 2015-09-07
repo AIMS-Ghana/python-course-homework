3db9e826d54a14c7cf6521eaa0e90732df4e0cbd
selom-aims <selom@aims.edu.gh>
2015-09-07 04:33:17 +0000

    updating calculusplotter

--- calculus_plotter.py
+++ calculus_plotter.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 from numpy import *
 
 from scipy.integrate import odeint
@@ -32,6 +31,27 @@ if __name__ == " __main__":
    plot_all(lambda x:x,x)
 
 
+
+
+import sys
+import pylab
+import numpy
+import fun_plots as fp
+from scipy.misc import derivative
+
+x = numpy.arange(0,10,0.01) # 100 linearly spaced numbers
+# compose plot
+
+def plotter(f):
+	pylab.figure()
+	df = derivative(f,x,dx=1,n=1)
+	
+	pylab.plot(x,f(x), 'b', label = "f(x)")
+	pylab.plot(x,df,'r', label = "f'(x)")
+	pylab.legend()
+	pylab.show() # show the plot
+
+plotter(numpy.cos)
 
 
 import sys
