3233bb266963e9ac4dc26e83c75008d38751ee5d
nabnel <mike@aims.edu.gh>
2015-09-07 23:59:42 +0000

    Done with shape objects

--- shape_objects.py
+++ shape_objects.py
@@ -8,13 +8,13 @@ class Shape:
         return area
 
     def dimname(self):
-        return "HUH?"
+        return "Side"
 
     def __init__(self, area):
         self.__area = area
         self.__dim = self.invert_area(area)
 
-    strformat = "{}, area: {}, {}: {}"
+    strformat = "{0}, area: {1}, {2}: {3}"
 
     def __str__(self):
         return Shape.strformat.format(
@@ -24,20 +24,49 @@ class Shape:
           self.__dim
         )
 
+    def check(self, area):
+        assert (area >=0 ), "Area cannot be negative"
+
+
     def draw(turt):
         pass
 
 class Triangle(Shape):
-    pass
+    def kind(self):
+        return "TRIANGLE"
+
+    def invert_area(self, area):
+        self.check(area)
+        return ((16*area**2)/3)**(0.25)
+
 
 class Square(Shape):
-    pass
+    def kind(self):
+        return "SQUARE"
+
+    def invert_area(self,area):
+        self.check(area)
+        return area**(0.5)
 
 from math import pi
 
+
 class Circle(Shape):
-    pass
+    def kind(self):
+        return "CIRCLE"
+
+    def invert_area(self, area):
+        self.check(area)
+        return (area/pi)**(0.5)
+
+    def dimname(self):
+        return "Radius"
+
 
 if __name__ == "__main__":
-    circ = Circle(10)
-    print(circ)
+    import sys
+    shapes = {"CIRCLE":Circle, "SQUARE":Square, "TRIANGLE":Triangle}
+    assert (len(sys.argv) == 3), "Input format not appropriate. Specify type, and then area of shape."
+    assert (sys.argv[1] in shapes ), "Shape type not understood"
+    given = shapes[sys.argv[1]](10)
+    print(given)
