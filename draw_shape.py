413bec8a299aaa171882d24114dfd439d919c7db
rhodarlin-aims <rhodarlin@aims.edu.gh>
2015-09-08 11:39:35 +0000

    turning in fun plots2

--- draw_shape.py
+++ draw_shape.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python
 
+
 import turtle
 
 from shapes import compute
 
-def Circle(radius):
-	turtle.circle(radius)
+def circle(radius):
+    turtle.circle(radius)
 
-def ngon(n,side):
-	turn= 180-(n-2)*180/n
-	
-    for i in range(n-1):
-	turtle.forward(side)
-	turtle.right(turn)
-	turtle.forward(side)
 
 def triangle(side):
-	ngon(3, side)
+    turtle.triangle(side)
 
-def square(side)
-	ngon(4, side)
+def square(side):
+    turtle.square(side)
 
-figs={
-	'CIRCLE':circle,
-	'SQUARE':square,
-	'TRIANGLE':triangle
+figs = {
+    'CIRCLE':circle,
+    'SQUARE':square,
+    'TRIANGLE':triangle
 }
 
-def draw(shape, area):
-	dim=compute(shape,area)
-	figs[shape](dim)
-	turtle. exitoniclick ()
+def draw(shape, area, fill="black", cont=False):
+    dim = compute(shape, area)
+    turtle.color(fill)
+    turtle.begin_fill()
+    figs[shape](dim)
+    turtle.end_fill()
+    if not cont:
+        turtle.exitonclick()
 
-import sys.
+import sys
 
-if __name__= "__main__":
-	shape = sys.argv[1]
-	area = float(sys.argv[1])
-	draw(shape,area)
-	print
-
-
+if __name__ == "__main__":
+    shape = sys.argv[1]
+    area = float(sys.argv[2])
+    draw(shape, area)
